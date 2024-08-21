2a27adab0f6abfb367439bdfdb37133a9cadbd4c
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-08-21 14:34:50 -0600

    updated changelog

--- client/src/components/Changelog.jsx
+++ client/src/components/Changelog.jsx
@@ -4,8 +4,9 @@ const Changelog = () => (
   <>
     <h2 id="changelog" className="heading">Changelog</h2>
     <h6>API version: 1.0.0</h6>
-    <p><span className="underline">June 12, 2024</span> - Initialized quick start guide and changelog documentation for the API. Completed functionality to perform GET requests for all yoga poses.</p>
+    <p><span className="underline">August 21, 2024</span> - Added responsive grid layout to App component for improved content alignment on small devices.</p>
     <p><span className="underline">June 25, 2024</span> - Included response status codes and their meanings.</p>
+    <p><span className="underline">June 12, 2024</span> - Initialized quick start guide and changelog documentation for the API. Completed functionality to perform GET requests for all yoga poses.</p>
   </>
 );
 
